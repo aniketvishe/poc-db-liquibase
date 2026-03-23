c04796fdf62403286001755beac32728f6539c66
pravin-dubey <pravin.dubey@teknopoint.in>
2026-03-23 16:47:26 +0530

    added db record

---
+++ scripts/Insert.sql
@@ -0,0 +1,3 @@
+INSERT INTO TESTE_LIQUIBASE (Id, Teste) VALUES (1, 'First Record');
+INSERT INTO TESTE_LIQUIBASE (Id, Teste) VALUES (2, 'Second Record');
+INSERT INTO TESTE_LIQUIBASE (Id, Teste) VALUES (3, 'Third Record');
