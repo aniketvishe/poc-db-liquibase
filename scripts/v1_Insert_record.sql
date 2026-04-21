f6a7f0ff5a38ff4376c4fdf13bd3676093ab746f
aniketvishe <141002698+aniketvishe@users.noreply.github.com>
2026-04-21 19:35:05 +0530

    Update v1_Insert_record.sql

--- scripts/v1_Insert_record.sql
+++ scripts/v1_Insert_record.sql
@@ -1,3 +1,7 @@
+--liquibase formatted sql
+
+--changeset aniket:1 splitStatements:true
+
 INSERT INTO EMP (ID, NAME) VALUES (1, 'First Record');
 INSERT INTO EMP (ID, NAME) VALUES (2, 'Second Record');
 INSERT INTO EMP (ID, NAME) VALUES (3, 'Third Record');
